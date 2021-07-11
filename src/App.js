ef2eb852bad7c10c157fb231314c42bea842c9ac
Kvark900 <kvant800@gmail.com>
2021-07-11 16:12:42 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useMemo, useRef, useState} from "react";
+import SockJS from 'sockjs-client'
+import Stomp from 'stompjs'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const BASE_URL = "http://localhost:8080";
+
+function App()
+{
+    const stompClient = useRef();
+    const [msgs, setMsgs] = useState([]);
+    const [msg, setMsg] = useState("");
+
+    useEffect(() => {
+        if (!stompClient.current) {
+            console.log("STOMP client resolved to null, connecting...")
+            stompClient.current = initializeStompClient()
+            connect()
+        }
+        else if (!stompClient.current.connected) {
+            console.log("Connection lost, connecting again...")
+            connect()
+        }
+    }, [])
+
+    function connect()
+    {
+        if (!stompClient.current) return
+        stompClient.current.connect({}, (frame) => {
+            console.log('Connected: ' + frame);
+            stompClient.current.subscribe(`/topic/messages`, message => {
+                console.log("Received message: " + JSON.parse(message.body).content);
+                setMsgs(prevMsgs => [...prevMsgs, JSON.parse(message.body)]);
+            });
+        });
+    }
+
+    function initializeStompClient()
+    {
+        const URL = BASE_URL + '/chat';
+        console.log("Initializing STOMP client on url: " + URL)
+        const socket = new SockJS(URL);
+        return Stomp.over(socket);
+    }
+
+    function sendMessage()
+    {
+        console.log("sending message");
+        stompClient.current.send(/*BASE_URL +*/ "/ws/message", {}, JSON.stringify({'content': msg}));
+    }
+
+    let list = useMemo(() => {
+        let msgsList = !msgs ? [] : msgs.map((msg, i) => {
+            return <div key={i}> {msg.content}</div>;
+        });
+        console.log("List for rendering is: " + msgsList)
+        return msgsList;
+    },[msgs]);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div>
+                    <input value={msg}
+                           onChange={e => setMsg(e.target.value)}/>
+                    <button onClick={event => sendMessage()}
+                            value="send"/>
+                </div>
+                <div id="msgs">
+                    {list}
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
